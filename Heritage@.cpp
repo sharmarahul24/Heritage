e2c159a65269f9e1a3a3a1e5df96e3000df041e9
sharmarahul24 <150116927+sharmarahul24@users.noreply.github.com>
2024-05-13 12:29:14 +0530

    Add files via upload

---
+++ Heritage@.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+ 
+class Vehicle {
+public:
+    Vehicle() 
+    { 
+        cout << "This is a Vehicle\n"; 
+    }
+};
+ 
+class FourWheeler {
+public:
+    FourWheeler()
+    {
+        cout << "This is a 4 wheeler Vehicle\n";
+    }
+};
+ 
+class Car : public Vehicle, public FourWheeler {
+};
+ 
+int main()
+{
+    Car obj;
+    return 0;
+}
